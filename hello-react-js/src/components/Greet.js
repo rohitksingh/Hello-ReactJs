03b4afe53691e7c114ca2388cd0eee106c4a6399
Rohit <r4rohit002@gmail.com>
2019-12-17 22:29:23 -0700

    Added a arrow function

--- hello-react-js/src/components/Greet.js
+++ hello-react-js/src/components/Greet.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
-function Greet(){
-    return <h1>Hello Rohit</h1>
-}
+// function Greet(){
+//     return <h1>Hello Rohit</h1>
+// }
+
+const Greet = () => <h1>Hi There, This is coming from arrow function</h1>
 
 export default Greet;
