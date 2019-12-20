8dacb553d72ea98e7ce909e16a5d25165801d805
Rohit <r4rohit002@gmail.com>
2019-12-20 03:18:01 -0700

    Added onClick function in Button

--- hello-react-js/src/components/MessageStateComponent.js
+++ hello-react-js/src/components/MessageStateComponent.js
@@ -2,17 +2,46 @@ import React, {Component} from 'react'
 
 class MessageStateComponent extends Component{
 
+
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //        title: "Click here"
+    //     }
+    //  }
+  
+    //  changeTitle = () => {
+    //     this.setState({ title: "New title" });
+    //  };
+  
+    //  render() {
+    //      return <h1 onClick={this.changeTitle}>{this.state.title}</h1>;
+    //  }
+
     constructor(){
         super()
         this.state = {
-            message: 'Hit subscribe button'
+            message: 'Subscribe to NewsLetter'
         }
     }
 
+    changeMessage = () =>{
+
+        this.setState({
+            message: 'Thank You'
+        })
+
+    }
+
     render(){
-        return <h1>{this.state.message}</h1>
+        return (
+            <div>
+                <h1>{this.state.message}</h1>
+                <button onClick = {this.changeMessage}>Subscribe</button>
+            </div>
+        )
     }
 
 }
 
-export default MessageStateComponent;
+export default MessageStateComponent
