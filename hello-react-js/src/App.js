cb7acb0d46745e386da39bba57603e940ef05772
Rohit <r4rohit002@gmail.com>
2019-12-20 00:15:27 -0700

    Created a new component with JSX

--- hello-react-js/src/App.js
+++ hello-react-js/src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
+import WelcomeClassComponent from './components/WelcomeClassComponent'
 
 function App() {
   return (
     <div className="App">
       <Greet/> 
       <Welcome/>
+      <WelcomeClassComponent/>
     </div>
   );
 }
---
+++ hello-react-js/src/components/WelcomeClassComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const WelcomeClassComponent = () =>{
+    return (
+        <div>
+            <h1>
+                This component is written in JSX
+            </h1>
+        </div>
+
+    )
+}
+
+export default WelcomeClassComponent;
